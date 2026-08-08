af89a76fd64ca8f7b43ebfc2fb984d174e82f603
sanketsborkar <59994415+sanketsborkar@users.noreply.github.com>
2026-08-08 09:01:17 -0400

    Add: two-integer-sum - submission-1

---
+++ Data Structures & Algorithms/two-integer-sum/submission-1.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer,Integer> complementMap = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+
+            if (complementMap.containsKey(complement)) {
+                return new int[] {complementMap.get(complement), i};
+            }
+
+            complementMap.put(nums[i], i);
+        }
+
+        throw new IllegalArgumentException("No solution found");
+    }
+}
